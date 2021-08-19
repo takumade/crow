e1fe518dc26465e7d70c6020d9b12fe80999e0e9
takunda <madechangu.takunda2gmail.com>
2021-08-19 11:34:07 +0200

    ✨ Added browser

---
+++ src/classes/browser.ts
@@ -0,0 +1,34 @@
+const puppeteer = require('puppeteer');
+
+export class Browser {
+
+    browser
+    page
+
+    async getBrowser(){
+        this.browser = await puppeteer.launch({ headless: false });
+    }
+
+    async goToPage(url:string, waitForSelector:string = undefined){
+        await this.getBrowser()
+        this.page = await this.browser.newPage();
+        await this.page.goto(url);
+
+        if (waitForSelector != null){
+            await this.page.waitForSelector(waitForSelector)
+        }
+        
+    }
+
+   async enterInput(selector:string, text:string, delay:number = 100){
+        await this.page.type(selector, text, { delay: delay });
+    }
+
+   async clickSelector(selector:string){
+        await this.page.click(selector)
+   }
+        
+
+
+
+}
