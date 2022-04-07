ea52047ef80aac9f4fc693c31a19855ed4bffc0f
Takunda Madechangu <madechangu.takunda@gmail.com>
2022-04-07 14:53:15 +0200

    Modified cindex

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,29 @@
-let username:string = "saibot222"
-let password:string = "67595911[n00b]"
-
 
 import { TwitterClient } from "./classes/twitter_client"
 import { Browser } from "./classes/browser"
 
-
-
-let browser = new Browser().init().then(
-    (browser) => {
-
+const start = async () => {
+    // Init the browser 
+    try{
+        let browser = new Browser()
+        await browser.init()
+            
         console.log("The browser is ready: ", browser)
+        
         let client = new TwitterClient(browser)
-        client.login(username, password).then(async (result) => {
-            console.log("Login result: ", result)
-            let tweets = await client.fetchTweets("https://twitter.com/home", 10)
-            console.log("Tweets: ", tweets)
+        let loginResult = await client.login()
 
-        })
+        if(loginResult){
+            // Do your stuff here
+           
 
+            
+        }
 
-
+    }catch(e){
+        console.log(e)
     }
-).catch(console.log)
+}
+
+
+start()
