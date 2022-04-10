4922976860c670c4d0eebffeab686e3f10ee502d
Takunda Madechangu <madechangu.takunda@gmail.com>
2022-04-10 13:44:25 +0200

    Add exports

---
+++ index.ts
@@ -0,0 +1,11 @@
+import { Browser } from "./src/classes/browser"
+import Tweet from "./src/classes/tweet"
+import { TwitterClient } from "./src/classes/twitter_client"
+import User from "./src/classes/user"
+
+
+
+exports.TwitterClient = TwitterClient 
+exports.Browser = Browser
+exports.Tweet = Tweet
+exports.User = User
