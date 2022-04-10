99716117539645c898d90870c130373d398ab249
Takunda Madechangu <madechangu.takunda@gmail.com>
2022-04-10 13:45:28 +0200

    Adds more constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,12 @@
-export const username = "username"
-export const password = "password"
+// Creds 
+export const username = "<username>"
+export const password = "<password>"
+
+// Timeouts (seconds)
+export const waitForElTimeout = 60000
+export const sendKeyMinTimeout = 600
+export const sendKeyMaxTimeout = 2000
+export const sleepMinTime = 2000
+export const sleepMaxTime = 6000
+export const fetchTweetsMinTimeout = 6000
+export const fetchTweetsMaxTimeout = 10000
